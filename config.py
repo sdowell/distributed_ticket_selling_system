9d4166ef098c78c384c5ffb9ae7cde1323943fd2
Metehan <m_ozten@yahoo.com>
2017-01-24 17:35:44 -0800

    Adding config class

---
+++ config.py
@@ -0,0 +1,37 @@
+
+class Config:
+
+    @staticmethod 
+    def from_file(filename):
+        kiosks, delay, numtickets = [], None, None
+        with open(filename) as cfile:
+            for line in cfile.readlines():
+                spl = line.split()
+                if spl[0] == 'kiosk':
+                     if len(spl) == 2:
+                        kiosks.append(('localhost', int(spl[1])))
+                     elif len(spl) == 3:
+                        kiosks.append(spl[1], int(spl[2]))
+                elif spl[0] == "tickets":
+                    numtickets = int(spl[1])
+                elif spl[0] == "delay":
+                    delay = int(spl[1])
+                if len(kiosks) == 0:
+                    print("Error: no kiosks found")
+                if delay is None:
+                    print("Warning: no delay declared in cfg file, assuming 0.")
+                    delay = 0
+                if numtickets == None:
+                    pass #Do something later?
+        return Cfg.__init__(kiosks, delay, tickets = numtickets)
+
+    def to_file(filename):
+        pass
+
+    def __init__(self, kiosks, delay, tickets = None):
+        self.kiosks = kiosks
+        self.delay = delay
+        self.tickets = tickets
+
+
+
