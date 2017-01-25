56a37635932c0ff99123226975f20811bfbcf888
Metehan <m_ozten@yahoo.com>
2017-01-24 18:27:01 -0800

    adding additional message type

--- message.py
+++ message.py
@@ -1,18 +1,28 @@
 import struct
 
 BUY_MESSAGE_CODE = 1
+BUY_MESSAGE_RESPONSE_CODE = 2
+BUY_SUCCESS = 1
+BUY_FAIL = 0
 
 class Message:
 
 	def __init__(self, data):
-		self.data_length  = len_data
+		self.data = data
+		self.data_length  = len(data)
 
 	def serialize(self):
 		pass
 
 	@staticmethod
-	def deserialize(self):
-		pass
+	def deserialize(self, data):
+		message_type = struct.unpack("!B",data[0:1])
+		if message_type == BUY_MESSAGE_CODE:
+			return BuyMessage.deserialize(data)
+		elif message_type == BUY_MESSAGE_RESPONSE_CODE:
+			return BuyMessageResponse.deserialize(data)
+		else: #other message types
+			pass
 
 
 class BuyMessage(Message):
@@ -22,6 +32,29 @@ class BuyMessage(Message):
 
 	def __init__(self, num_tickets):
 		self.num_tickets = num_tickets
-		self.data = self.serialize()
-		super(BuyMessage, self).__init__(self.data)
+		super(BuyMessage, self).__init__(self.serialize())
+
+	@staticmethod
+	def deserialize(self, data):
+		num_tickets = struct.unpack("!I",data[1:5])
+		return BuyMessage(num_tickets)
+
+class BuyMessageResponse(Message):
+
+	def serialize(self):
+		return struct.pack("!BI", BUY_MESSAGE_RESPONSE_CODE, self.success)
+
+	def __init__(self, successful):
+		if successful is True:
+			self.success = BUY_SUCCESS
+		else:
+			self.success = BUY_FAIL
+		super(BuyMessageResponse, self).__init__(self.serialize())
+	
+	@staticmethod
+	def deserialize(self, data):
+		success = struct.unpack("!I", data[1:5])
+		return BuyMessageResponse(success == BUY_SUCCESS)
+	
+
 
